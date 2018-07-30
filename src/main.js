0afe0a92a9724b97939318d4f0eb47bd347e53ff
Sander <sander@funkhaus.us>
2018-07-30 09:32:41 -0400

    Adding vuex

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from '@/utils/router'
+import store from '@/utils/store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
 })
