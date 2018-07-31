9ad582a57dc139e9870d7d618bf1736066d1ddea
Sander <sander@funkhaus.us>
2018-07-31 09:47:24 -0400

    Sketching out more Partition coordinate systems

--- src/board/partition.js
+++ src/board/partition.js
@@ -1,5 +1,17 @@
 export default class {
-    constructor() {
-        this.width = 5
+    constructor(opts = {}) {
+        // default picker
+        function pick(name, defaultVal) {
+            return opts.hasOwnProperty(name) ? opts[name] : defaultVal
+        }
+
+        this.width = pick('width', 5)
+        this.height = pick('height', 5)
+
+        // pivot Cell in x,y coordinates
+        this.pivot = pick('pivot', [0, 0])
+
+        // position on game board in x,y coordinates
+        this.position = pick('position', [0, 0])
     }
 }
