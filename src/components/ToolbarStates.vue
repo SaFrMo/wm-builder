1b803f971f6bb48c01af0f3ac97f75a29d2ecaf1
Sander <sander@funkhaus.us>
2018-09-05 17:57:43 -0400

    Starting on board state toolbar

--- src/components/ToolbarStates.vue
+++ src/components/ToolbarStates.vue
@@ -4,6 +4,12 @@
 
         <h2 class="title">Board States</h2>
 
+        <ul class="button-wrap">
+            <button
+                @click="addState"
+                aria-label="Add board state">+</button>
+        </ul>
+
     </aside>
 
 </template>
@@ -28,6 +34,29 @@ export default {}
         transform: rotate(-0.25turn);
         transform-origin: top left;
         font-size: 30px;
+    }
+    .button-wrap {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        margin: 0 0 0 60px;
+        padding: 10px 0;
+        box-sizing: border-box;
+
+        button {
+            width: 100px;
+            height: 100px;
+            background-color: rgba($black, 0.2);
+            color: $white;
+            border-radius: 15px;
+
+            &:hover,
+            &:focus {
+                background-color: rgba($black, 0.4);
+            }
+        }
+        button:last-child {
+        }
     }
 }
 </style>
